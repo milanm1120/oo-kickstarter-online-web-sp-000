cfc5960e5a38260fe07ea3e4f0bdadf512b1a613
Milan Mody <milanm1120@gmail.com>
2021-03-21 15:07:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,10 @@
 class Backer
 
   attr_reader :name
-  @backed_projects = []
 
   def initialize(name)
     @name = name
-    @backed_projects << name
+    @backed_projects = Array.new
   end
 
 end
